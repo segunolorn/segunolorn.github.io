ec4255bb360f6a6c9093b028f92710ae1a359e56
segunolorn <109775284+segunolorn@users.noreply.github.com>
2024-02-21 22:57:56 +0100

    Add files via upload

---
+++ HTML/greetings.js
@@ -0,0 +1,13 @@
+export const informalGreeting = (person) => {
+    console.log(`Hello, ${person}`);
+}
+
+export const formalGreetimg = (person) => {
+    console.log(`Good day, ${person}`);
+}
+
+const greeting = () => {
+    console.log(`Howdy!`);
+}
+
+export default greeting;
